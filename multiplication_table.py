fa9391665b128f664fd55522d7723fab9c563628
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 14:21:05 -0500

    added multiplication table program

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,7 +1,8 @@
 i = 1
 j = 1
 
-for i in range(1,11):
+for i in range(1,11):#incrementing the 1st number
     for j in range(1,11):
-        print("%d X %d" % (i,j))
+        print("%d X %d" % (i,j))#incrementing the second number 1st X 2nd
+    print()#printing a line break after 10
       
