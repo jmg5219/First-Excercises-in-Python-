e11081a0023f4260719fb82dedc990300ca58365
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 13:04:21 -0500

    added error handling for input

--- celsius_to_farenheit.py
+++ celsius_to_farenheit.py
@@ -1,3 +1,6 @@
-temp_c = int(input("Temperature in C?"))
-temp_f = (temp_c*(9/5))+32
-print("%.1f F" % (temp_f))
+try:
+    temp_c = int(input("Temperature in C?"))
+    temp_f = (temp_c*(9/5))+32
+    print("%.1f F" % (temp_f))
+except:
+    print('please verify numeric input and try again')
