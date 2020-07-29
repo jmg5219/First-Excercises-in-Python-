331c649e9518d6f9fb747953edc8438adc5d1d4e
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-28 20:28:07 -0500

    working with 2D lists

---
+++ print_a_square.py
@@ -0,0 +1,10 @@
+square_size = int(input("How big is the square?"))
+i = square_size 
+j = square_size     
+for i in range(1, i+1) : 
+    for j in range(1, j+1) : 
+        print("*", end="")                  
+    print() 
+
+  
+  
