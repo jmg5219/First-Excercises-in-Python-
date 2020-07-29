09ea54357f2093b3b1fc706908abb19ca512e1af
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-28 19:19:31 -0500

    introduced input error handling

--- n_to_m.py
+++ n_to_m.py
@@ -1,10 +1,13 @@
 start = int(input("Start From:"))
 end = int(input("End on:"))
 
-i = start - 1 
-while i < end:
-    i += 1
-    print(i)
+if start < end:
+    i = start - 1 
+    while i < end:
+        i += 1
+        print(i)
+elif start > end:
+    print("Please restart the program and verify that start number is less than the end number")
 
 
 
