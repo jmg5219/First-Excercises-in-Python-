bed6f86c86c666cd677a3e84dda010dcaa358cd2
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 11:18:44 -0500

    added random number functionality

--- conditionals.py
+++ conditionals.py
@@ -1,14 +1,13 @@
-user_input = int(input("Guess a number 5-50: "))
-magic_number = 6
+def number_game():
+    user_input = int(input("Guess a number 5-50: "))
+    magic_number = random.randint(5,50)
+    print(magic_number)
+    if user_input == magic_number:
+        print("ARE YOU A MIND READER!?!?")
+    elif (user_input > 50) or (user_input < 5):
+        print("Guess was out of bounds")
+    else: 
+        print("Sorry, try again")
 
-if user_input == magic_number:
-    print("ARE YOU A MIND READER!?!?")
-elif user_input > 50:
-    print("Guess was too high")
-elif user_input < 5:
-    print("Guess was too low")
-else: 
-    print("Sorry, try again")
-
-
-
+import random
+number_game()
