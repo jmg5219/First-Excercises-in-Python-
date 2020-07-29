9fb0d69505b4c9d4c9d7cab9e8f4a1f863130b6e
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 15:48:37 -0500

    program to find smallest number

---
+++ smallest_number.py
@@ -0,0 +1,8 @@
+num_list = [1,2,3,4,5,6,7,8,9,10]
+# min_num = max(num_list)
+
+# for i in range(0,len(num_list)):
+#     if min_num > num_list[i] :
+#         min_num = num_list[i]
+# print (min_num)
+print(min(num_list))
