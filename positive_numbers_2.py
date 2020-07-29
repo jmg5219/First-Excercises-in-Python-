64f3fa3a04ce28bba2307adf5f7bf474a77fc047
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 15:47:45 -0500

    program to print positive numbers in a new list

---
+++ positive_numbers_2.py
@@ -0,0 +1,11 @@
+num_list = [-2,-1,0,1,2]
+pos_nums = []
+j = 0
+for i in range(0,len(num_list)):
+    if num_list[i] > 0:
+        pos_nums.append(num_list[i])
+
+for j in range(0,len(pos_nums)):
+    print(pos_nums[j])
+
+        
