3ea53812c94bf5ba3bf8f866e30a6e5142be7368
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 11:54:18 -0500

    program to explore conditionals

--- conditionals.py
+++ conditionals.py
@@ -1,13 +1,17 @@
-def number_game():
-    user_input = int(input("Guess a number 5-50: "))
-    magic_number = random.randint(5,50)
-    print(magic_number)
-    if user_input == magic_number:
-        print("ARE YOU A MIND READER!?!?")
-    elif (user_input > 50) or (user_input < 5):
-        print("Guess was out of bounds")
-    else: 
-        print("Sorry, try again")
+def number_game(magic_number):
+    user_input = 0
+    while user_input != magic_number:
+        user_input = int(input("Guess a number 5-50: "))
+        #print(magic_number)
+        if user_input == magic_number:
+            print("ARE YOU A MIND READER!?!?")
+        elif (user_input > 50) or (user_input < 5):
+            print("Guess was out of bounds")
+        elif user_input < magic_number: 
+            print("Sorry, try a higher number")
+        elif user_input > magic_number:
+            print("Sorry, try a lower number")
 
 import random
-number_game()
+magic_number = random.randint(5,50)
+number_game(magic_number)
