27370b93d6c7710f842512b878b8893c09c0c25b
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 10:51:53 -0500

    added some more conditional statements

--- conditionals.py
+++ conditionals.py
@@ -1,8 +1,12 @@
-user_input = int(input("Guess a number: "))
+user_input = int(input("Guess a number 5-50: "))
 magic_number = 6
 
 if user_input == magic_number:
     print("ARE YOU A MIND READER!?!?")
+elif user_input > 50:
+    print("Guess was too high")
+elif user_input < 5:
+    print("Guess was too low")
 else: 
     print("Sorry, try again")
 
