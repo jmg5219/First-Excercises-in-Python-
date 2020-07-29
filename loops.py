ffad3109ed2e134a62437232d0b70e09d9873ebb
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 11:54:37 -0500

    program to investigate loops

---
+++ loops.py
@@ -0,0 +1,21 @@
+title = "Green Lantern Corp."
+STOP = 10 
+counter = 0 
+# Prints all letters
+# while counter < len(title):
+#     #print(counter)
+#     print(title[counter])
+#     counter += 1
+
+#Prints Even Letters and no spaces
+while counter < len(title):
+    if (counter % 2) == 0 and title[counter] != ' ':
+        print(title[counter])
+    counter += 1
+
+#Prints Odd Letters
+# while counter < len(title):
+#     if (counter % 2) != 0:
+#         print(title[counter])
+#         #print(counter)
+#     counter += 1
