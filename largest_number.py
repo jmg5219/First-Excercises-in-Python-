2c5b4a8dc8e9f6601a2df4e70667cd890f5355b9
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 15:46:26 -0500

    program to print even numbers

---
+++ largest_number.py
@@ -0,0 +1,8 @@
+num_list = [1,2,3,4,5,6,7,8,9,10]
+# max_num = 0
+
+# for i in range(0,len(num_list)):
+#     if max_num < num_list[i] :
+#         max_num = num_list[i]
+# print (max_num)
+print(max(num_list))
