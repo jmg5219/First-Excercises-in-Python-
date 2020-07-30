7079a8c81e25d2a96d96b6186bcb16eeeb65a792
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-29 19:47:54 -0500

    updated program to handle restarting the game, limiting the number of guesses and handling non-numeric entry handling

--- conditionals.py
+++ conditionals.py
@@ -1,17 +1,47 @@
-def number_game(magic_number):
-    user_input = 0
-    while user_input != magic_number:
-        user_input = int(input("Guess a number 5-50: "))
-        #print(magic_number)
-        if user_input == magic_number:
-            print("ARE YOU A MIND READER!?!?")
-        elif (user_input > 50) or (user_input < 5):
-            print("Guess was out of bounds")
-        elif user_input < magic_number: 
-            print("Sorry, try a higher number")
-        elif user_input > magic_number:
-            print("Sorry, try a lower number")
-
 import random
-magic_number = random.randint(5,50)
-number_game(magic_number)
+magic_number = random.randint(1,10)
+user_input = 0 
+guess = 0 
+total_guess = 5
+remain_guess = total_guess - guess
+play_again = 'Y'
+
+
+while play_again == 'Y':
+    if play_again == 'Y':
+        remain_guess = total_guess - guess
+        if remain_guess > 0 :
+    
+            print("I am thinking of a number between 1 and 10.")
+            #print(magic_number)
+            guess += 1
+            print('You have %d guesses left.' % (remain_guess))
+            try:
+                user_input = int(input("What's the number? "))
+            except:
+                print('Please enter a numeric value for your guess')
+            if user_input == magic_number:
+                print('Yes! You win!')
+                print()
+                play_again = input('Do you want to play again (Y or N)?')
+                if play_again == 'N' :
+                    print('Bye!')
+                if play_again == 'Y':
+                    total_guess = 5
+                    guess = 0
+            if user_input > magic_number :
+                print ('Try a smaller number')
+                print()
+            if user_input < magic_number :
+                print('Try a larger number')
+                print()
+        elif remain_guess == 0:
+            play_again = input('Do you want to play again (Y or N)?')
+            if play_again == 'N' :
+                print('Bye!')
+            if play_again == 'Y':
+                total_guess = 5
+                guess = 0
+                remain_guess = total_guess - guess
+    elif play_again == 'N' :
+        print('Bye!')
