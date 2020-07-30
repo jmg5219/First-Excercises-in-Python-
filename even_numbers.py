52985e3de6bc3de3a74c1813885b40e1d0ba6241
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-30 14:29:24 -0500

    updated with function

--- even_numbers.py
+++ even_numbers.py
@@ -1,6 +1,7 @@
+def even_nums(num_list):
+    for i in range(0,len(num_list)):
+        if (num_list[i] % 2) == 0:
+            print(num_list[i])
+
 num_list = [1,2,3,4,5,6,7,8,9,10]
-j = 0
-for i in range(0,len(num_list)):
-    if (num_list[i] % 2) == 0:
-        print(num_list[i])
-        j += 1
+even_nums(num_list)
