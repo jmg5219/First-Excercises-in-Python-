3cda0845dd809216637608ca76da428a88cfa518
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-30 11:30:24 -0500

    another matrix addition program varying size

---
+++ matrix_addition_II.py
@@ -0,0 +1,21 @@
+mat_1 = [[1,3,3],[2,4,4],[1,2,7]]
+mat_2 = [[5,2,2],[1,0,6],[3,4,8]]
+
+rows = len(mat_1)
+columns = len(mat_1[0])
+
+#print(rows)
+#print(columns)
+
+mat_3 = []
+
+for i in range(rows):
+    for j in range(columns):
+        #mat_3[i][j] = mat_1[i][j]+mat_2[i][j]
+        mat_3[i][j]= 0
+
+print(mat_3)
+
+
+        
+
