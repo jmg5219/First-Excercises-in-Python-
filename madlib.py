fc0961e041ce2de3fd70484c247d4d0c7fc79bca
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-30 13:42:10 -0500

    introduced a function to perform string interpolation

--- madlib.py
+++ madlib.py
@@ -1,5 +1,9 @@
+def madlib(name,subject):
+    the_madlib = "Your name is %s and your favorite subject is %s" % (name,subject)
+    return the_madlib
+
 print("Please fill in the blanks:")
 print("____(name)____'s favorite subject in school is ____(subject)____.")
 name = input("What is your name?")
 subject = input("What is your favorite subject?")
-print(name+"'s "+"favorite subject in school is " + subject)
+print(madlib(name,subject))
