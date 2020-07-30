5dd6431913d5ed88a2a5aa84bbf70355794f572e
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-30 13:55:18 -0500

    added function for calculation

--- celsius_to_farenheit.py
+++ celsius_to_farenheit.py
@@ -1,6 +1,16 @@
-try:
-    temp_c = int(input("Temperature in C?"))
+# try:
+#     temp_c = int(input("Temperature in C?"))
+#     temp_f = (temp_c*(9/5))+32
+#     print("%.1f F" % (temp_f))
+# except:
+#     print('please verify numeric input and try again')
+
+
+def convert_F(temp_c):
     temp_f = (temp_c*(9/5))+32
-    print("%.1f F" % (temp_f))
-except:
-    print('please verify numeric input and try again')
+    return temp_f
+    
+
+temp_c = int(input("Temperature in C?"))
+
+print("%.1f F" % (convert_F(temp_c)))
