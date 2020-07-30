310fa105598b5ff16d7354570a2518c37867ce3b
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-30 14:00:14 -0500

    programs for temperature conversion

--- farenheit_to_celsius.py
+++ farenheit_to_celsius.py
@@ -6,12 +6,10 @@
 #     print('please verify numeric input and try again')
 
 
-def convert_F(temp_c):
-    temp_f = (temp_c*(9/5))+32
-    return temp_f
+def convert_c(temp_f):
+    temp_c = (temp_f-32)*(5/9)
+    return temp_c
     
 
-temp_c = int(input("Temperature in C?"))
-print("%.1f F" % (convert_F(temp_c)))
-
-
+temp_f = int(input("Temperature in F?"))
+print("%.1f C" % (convert_c(temp_f)))
